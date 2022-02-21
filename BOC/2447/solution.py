78f25ef26d24701faea587e8e488fcf889c913a2
Yusun <solarpy@hanyang.ac.kr>
2022-02-21 23:18:44 +0900

    미완) 2447 : 별 찍기 - 10

---
+++ BOC/2447/solution.py
@@ -0,0 +1,15 @@
+def tri_star():
+    print("***")
+    print("* *")
+    print("***")
+
+def black():
+    print("    ")
+    print("    ")
+    print("    ")
+
+
+n = int(input())
+
+for x in range(n):
+    

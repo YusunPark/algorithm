a38cf8dacddce3f4857bced6c4686c6696e1b7cd
Yusun <solarpy@hanyang.ac.kr>
2022-03-02 02:27:25 +0900

    24511 : queuestack

--- BOC/24511/solution.py
+++ BOC/24511/solution.py
@@ -1,32 +1,15 @@
 from sys import stdin
-# n = int(input())
 n = int(stdin.readline())
+qs = list(map(int,stdin.readline().split()))
+container = list(stdin.readline().split())
 
-# qs = list(map(int, input().split(" ")))
-qs = list(map(int, stdin.readline().split()))
-
-# container = list(map(int, input().split(" ")))
-container = list(map(int, stdin.readline().split()))
-
-
-# m = input()
 m = int(stdin.readline())
-
-# c = list(map(int, input().split(" ")))
-c = list(map(int, stdin.readline().split(" ")))
-
-array = [0]*m
-
-for i in range(m):
-    result = c[i]
-    for j in range(n):
-        if qs[j] == 1:
-            pass
-        else:
-            result, container[j] = container[j], result
-    array[i] = result
-
-s = ""
-for i in array:
-    s += (str(i) + ' ')
-print(s)
+c = list(stdin.readline().split(" "))
+    
+queue = []
+for i in range(n):
+    if qs[i] == 0:
+        queue.append(container[i])
+queue.reverse()
+queue.extend(c)
+print(" ".join(queue[:m]))

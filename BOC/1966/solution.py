cd5f0a632d4fe783bb7484ea8616838b3e31762a
Yusun <solarpy@hanyang.ac.kr>
2022-03-15 17:26:21 +0900

    1966 : 프린터 큐

--- BOC/1966/solution.py
+++ BOC/1966/solution.py
@@ -1,48 +1,51 @@
-# 자신보다 우선순위가 높은게 있다면 -> 해당 문서를 제일 뒤로
-
-# 1. 총 테스트 횟수
-# 2. 문서의 개수 + 궁금한 문서가 어디에 있는지
-# 3. n개 문서의 중요도
 from queue import Queue
 
-
-def findMax(array, value):
-    for x in array:
-        if x > value:
-            value = x
-        else: pass
-    return value
-
-def outQueue(queue, priority, cnt):
-    value = queue.get()
-    max_value = findMax(priority, value)
-    while(1):
-        if  value == max_value:
-            cnt += 1
-            return value, cnt
-
-        else:
-            queue.put(value)
-            value = queue.get()
-
+# 결과를 저장할 result 추가
 result = ''
 
+# 매 입력이 들어올 때마다 for문을 실행 (테스트 케이스의 갯수)
 for _ in range(int(input())):
-    cnt = 0
+
+    # 입력
     n, m = map(int, input().split(" "))
     priority = list(map(int, input().split(" ")))
+
+    # 큐 : 삽입 삭제가 o(1)
+    # priority에 입력한 값들을 큐에 집어넣는 작업 
+    # : 이때 인덱스도 저장해서 동일 값이 여러개 있을 경우 구분하는 것으로 사용한다.
+    # (priority를 이용하지 않고 바로 큐에 넣는 방법을 모르겠습니다..)
     queue = Queue()
-
     for i in range(n):
-        queue.put(priority[i])
+        queue.put((priority[i], i))
 
+    # 최대값 찾는 과정을 빠르게 하기 위한 작업 (정렬을 이용)
+    sort_priority = priority.copy()
+    sort_priority.sort(reverse=True)
+
+    # 몇번째 출력인지 저장할 변수
+    cnt = 0 
+
+    # sort_priority 에서 사용할 인덱스를 따로 만들어줌 (반복문을 덜 쓰기 위해서)
+    max_index = 0
+
+    # 실제로 작업을 하는 구간
     while(1):
-        value, cnt = outQueue(queue, priority, cnt)
-        if value == priority[m]:
-            result += str(cnt) + '\n'
-            break
+        max_value = sort_priority[max_index]
+        value, index = queue.get()
+
+        # 최대값과 현재 queue에서 나온 값이 같지만 인덱스가 다름 : 빠진걸 다시 넣음
+        # 최대값과 현재 queue에서 나온 값이 같고, 인덱스도 같음 : break로 반복문 탈출 및 다음 테스트케이스 실행
+        if value == max_value :
+            if index != m:
+                cnt += 1
+                max_index += 1
+                queue.put((value, index))
+            else: 
+                cnt += 1
+                result += str(cnt) + '\n'
+                break
         else:
-            priority.remove(value)
+            queue.put((value, index))
 
+# 결과 값 출력 (마지막에 \n 이 하나 있어서 슬라이싱 진행)
 print(result[:-1])
-

63137796b334a6baf0a4c125548c06c600de9f81
YusunPark <solarpy.kor@gmail.com>
2022-10-08 01:20:10 +0900

    42578 : 위장

--- programmers/42578/solution.py
+++ programmers/42578/solution.py
@@ -1,19 +1,21 @@
-from collections import Counter
-from functools import reduce
-
-
 def solution(clothes):
-    return reduce(
-        lambda x,y : x * (y + 1),
-        Counter(x for _, x in clothes).values(), 1
-    ) - 1
-
+    dict_ = {}
+    for x, y in clothes:
+        if y in dict_:
+            dict_[y] += 1
+        else:
+            dict_[y] = 1
+        
+    answer = 1
+    for val in dict_.values():
+        answer *= val+1
+    
+    return answer-1
 
 result = solution([
     ["yellow_hat", "headgear"],
     ["blue_sunglasses", "eyewear"],
     ["green_turban", "headgear"]
 ])
-
 
 print(result)

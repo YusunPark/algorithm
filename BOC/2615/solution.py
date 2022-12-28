1c247e73bd92981ba25f4095d90ab95bd1c9c08d
YusunPark <solarpy.kor@gmail.com>
2022-12-29 00:49:06 +0900

    2615 : 오목 is_range() 함수 추가로 human error 제거

--- BOC/2615/solution.py
+++ BOC/2615/solution.py
@@ -5,43 +5,46 @@ board = [
     for _ in range(19)
 ]
 
+def is_range(y, x):
+    return 0 <= x < 19 and 0 <= y < 19
+
 def check_right(y, x, value):
-    if x+4 < 19 and min(board[y][x:x+5]) == max(board[y][x:x+5]) == value:
-        if x+5 < 19 and board[y][x+5] == value: return False
-        elif x-1 >= 0 and board[y][x-1] == value: return False
+    if is_range(y, x+4) and min(board[y][x:x+5]) == max(board[y][x:x+5]) == value:
+        if is_range(y, x+5) and board[y][x+5] == value: return False
+        elif is_range(y, x-1) and board[y][x-1] == value: return False
         return True
     return False
 
 def check_down(y, x, value):
-    if y+4 < 19:
+    if is_range(y+4, x):
         tmp = []
         for i in range(5):
             tmp.append(board[y+i][x])
         if min(tmp) == max(tmp) == value:
-            if y+5 < 19 and board[y+5][x] == value: return False
-            elif y-1 >= 0 and board[y-1][x] == value: return False
+            if is_range(y+5, x) and board[y+5][x] == value: return False
+            elif is_range(y-1,x) and board[y-1][x] == value: return False
             return True
     return False
 
 def check_r_diagonal(y, x, value):
-    if x+4 < 19 and y+4 < 19:
+    if is_range(y+4, x+4):
         tmp = []
         for i in range(5):
             tmp.append(board[y+i][x+i])
         if min(tmp) == max(tmp) == value:
-            if x+5 < 19 and y+5 < 19 and board[y+5][x+5] == value: return False
-            elif x-1 >= 0 and y-1 >= 0 and board[y-1][x-1] == value: return False
+            if is_range(y+5, x+5) and board[y+5][x+5] == value: return False
+            elif is_range(y-1,x-1) and board[y-1][x-1] == value: return False
             return True
     return False
 
 def check_l_diagonal(y, x, value):
-    if x-4 >= 0 and y+4 < 19:
+    if is_range(y+4, x-4):
         tmp = []
         for i in range(5):
             tmp.append(board[y+i][x-i])
         if min(tmp) == max(tmp) == value:
-            if x-5 >= 0 and y+5 < 19 and board[y+5][x-5] == value: return False
-            elif x+1 < 19 and y-1 >= 0 and board[y-1][x+1] == value: return False
+            if is_range(y+5, x-5) and board[y+5][x-5] == value: return False
+            elif is_range(y-1,x+1) and board[y-1][x+1] == value: return False
             return True
     return False
 
